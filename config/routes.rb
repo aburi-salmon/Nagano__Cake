f17afbcc7587f5eab18ce5688291a0d034a18a1b
EC2 Default User <ec2-user@ip-172-31-87-240.ec2.internal>
2021-09-16 05:45:54 +0000

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
+  get 'search' => 'searches#search'
+  get 'members/confirm' => 'members#confirm'
+  patch 'menbers/withdraw' => 'members#withdraw'
+  delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+  post 'orders/confirm' => 'orders#confirm'
+  get 'orders/complete' => 'orders#complete'
+  resources :members, only: [:edit, :update, :show]
+  resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+  resources :products, only: [:index, :show] do
+    resources :cart_items, only: [:index, :create, :destroy, :edit]
+  end
+  resources :orders, only: [:new, :create, :index, :show]
+  namespace :admin do
+    root to: 'homes#top'
+    resources :members, only: [:index, :edit, :update, :show]
+    resources :products, except: [:destroy]
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: [:update]
+    end
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
   devise_for :members
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admins
 end
