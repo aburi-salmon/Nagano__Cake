aa73837895d5b6d9803fa7e1ab0cb8dc9d8193a0
EC2 Default User <ec2-user@ip-172-31-80-45.ec2.internal>
2021-09-20 04:58:08 +0000

    Add cart_item2

--- app/controllers/publics/cart_items_controller.rb
+++ app/controllers/publics/cart_items_controller.rb
@@ -4,6 +4,7 @@ class Publics::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_member.cart_items
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
 
   def create
@@ -12,17 +13,30 @@ class Publics::CartItemsController < ApplicationController
     @cart_item.save
     redirect_to cart_items_path
   end
-  
-  def destoy
+
+  def update
+    @cart_items = CartItem.find(params[:id])
+    @cart_items.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+
+  def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_items_path
   end
 
+  def destroy_all
+    member = Member.find(current_member.id)
+    member.cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
+
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:produ_id, :member_id)
+    params.require(:cart_item).permit(:product_id, :member_id, :quantity)
   end
 
 end
